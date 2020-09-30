22804a9f4bcc6c7cba805057cadc9dd424b50d4a
WillamyCosta <willamycosta@hotmail.com>
2020-09-30 00:11:51 -0300

    Nome de usuario

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -39,7 +39,7 @@ const MainHeader: React.FC = () => {
 
             <Profile>
                 <Welcome>Olá, {emoji}</Welcome>
-                <UserName>Rodrigo Gonçalves</UserName>
+                <UserName>Willame Costa</UserName>
             </Profile>
         </Container>
     );
